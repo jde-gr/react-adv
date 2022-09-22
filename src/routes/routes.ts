cf73dabae95cdc4548f0b73754669b05681a47d8
Francisco Javier Delgado Esteban <javier.delgadoesteban@plexus.es>
2022-09-22 10:58:33 +0200

    Rutas anidadas listas

---
+++ src/routes/routes.ts
@@ -0,0 +1,42 @@
+import { lazy, LazyExoticComponent } from 'react';
+// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
+
+type JSXComponent = () => JSX.Element;
+
+interface Route {
+  to: string;
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
+}
+
+const LazyLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout'
+    )
+);
+const Lazy2 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
+);
+const Lazy3 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+);
+
+export const routes: Route[] = [
+  {
+    to: '/lazyload/',
+    path: '/lazyload/*',
+    Component: LazyLayout,
+    name: 'Lazy-1',
+  },
+  {
+    to: '/no-lazy',
+    path: 'no-lazy',
+    Component: NoLazy,
+    name: 'No Lazy',
+  },
+];
