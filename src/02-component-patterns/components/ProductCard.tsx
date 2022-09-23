2b5fff53e1066748f855aa70068ac8bf61ff540b
Francisco Javier Delgado Esteban <javier.delgadoesteban@plexus.es>
2022-09-23 10:01:42 +0200

    Component Extend Styles listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,19 +1,25 @@
 import { createContext } from 'react';
 import styles from '../styles/styles.module.css';
 import { useProduct } from '../hooks/useProduct';
-import {
-  ProductCardProps,
-  ProductContextProps,
-} from '../interfaces/interfaces';
+import { Product, ProductContextProps } from '../interfaces/interfaces';
+
+export interface Props {
+  product: Product;
+  children?: React.ReactElement | React.ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+}
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
+export const ProductCard = ({ children, product, className, style }: Props) => {
   const { counter, increaseBy } = useProduct();
   return (
     <Provider value={{ counter, increaseBy, product }}>
-      <div className={styles.productCard}>{children}</div>
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children}
+      </div>
     </Provider>
   );
 };
--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -3,7 +3,13 @@ import { ProductContext } from './ProductCard';
 import noImage from '../assets/no-image.jpg';
 import styles from '../styles/styles.module.css';
 
-export const ProductImage = ({ img = '' }) => {
+export interface Props {
+  img?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductImage = ({ img, className, style }: Props) => {
   const { product } = useContext(ProductContext);
   let imgToShow: string = noImage;
   if (img) {
@@ -11,5 +17,12 @@ export const ProductImage = ({ img = '' }) => {
   } else if (product.img) {
     imgToShow = product.img;
   }
-  return <img className={styles.productImg} src={imgToShow} alt='Product' />;
+  return (
+    <img
+      className={`${styles.productImg} ${className}`}
+      style={style}
+      src={imgToShow}
+      alt='Product'
+    />
+  );
 };
